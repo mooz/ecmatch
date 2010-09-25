f6b8c847de08daeae5ddf070e5a9bf7f2b41e4cf
mooz <stillpedant@gmail.com>
2010-09-25 15:54:17 +0900

    Implemented two-column

--- test/interactive.js
+++ test/interactive.js
@@ -10,113 +10,61 @@
         }
     };
 
-    function serialize(obj, level, noIndent) {
-        const INDENT_LEVEL = 4;
-
-        if (!level)
-            level = 0;
-
-        function indentation(i) {
-            return (new Array(i)).join(" ");
-        }
-
-        function indent(str) {
-            return (noIndent ? "" : indentation(level)) + (str || "");
-        }
-
-        function nextIndentLevel() {
-            return level + INDENT_LEVEL;
-        }
-
-        function escapeKey(k) {
-            return k.replace(/"/g, '\\"');
-        }
-
-        var str = null;
-
-        var type = typeof obj;
-
-        if (obj === null) {
-            str = indent("null");
-        } else if (type === "string") {
-            str = indent('"' + escapeKey(obj) + '"');
-        } else if (type === "number" ||
-                   type === "boolean") {
-            str = indent(obj.toString());
-        } else if (obj instanceof Array) {
-            var buffer = [];
-
-            buffer.push("[");
-
-            for (var i = 0; i < obj.length; ++i) {
-                var item = serialize(obj[i], nextIndentLevel());
-
-                if (item)
-                    buffer.push(item);
-            }
-
-            for (var i = 1; i < buffer.length; ++i)
-                if (i !== buffer.length - 1)
-                    buffer[i] += ",";
-
-            buffer.push(indentation(level) + "]");
-
-            str = buffer.map(function (s, i) { return i ? s : indent(s); }).join("\n");
-        } else if (type === "object") {
-            // object
-            var buffer = [];
-
-            buffer.push("{");
-
-            for (var k in obj) if (obj.hasOwnProperty(k)) {
-                var value = serialize(obj[k], nextIndentLevel(), true);
-
-                if (value)
-                    buffer.push(indentation(nextIndentLevel()) + '"' + escapeKey(k) + '": ' + value);
-            }
-
-            for (var i = 1; i < buffer.length; ++i)
-                if (i !== buffer.length - 1)
-                    buffer[i] += ",";
-
-            buffer.push(indentation(level) + "}");
-
-            str = buffer.map(function (s, i) { return i ? s : indent(s); }).join("\n");
-        } else {
-            // undefined
-        }
-
-        return str;
-    }
-
     window.addEventListener("load", function () {
-        var inputArea  = document.getElementById("input");
-        var resultArea = document.getElementById("result");
+        var resultArea           = document.getElementById("result");
+        var patternStructureArea = document.getElementById("pattern-structure");
 
-        removeAllChild(resultArea);
-        var dummyHolder = document.createElement("div");
-        resultArea.appendChild(dummyHolder);
+        var patternInputArea = document.getElementById("input-pattern");
+        var valueInputArea   = document.getElementById("input-value");
 
-        function echo(elem, klass) {
+        function echoTo(txt, target, klass) {
+            var elem = E.text(txt);
+
+            removeAllChild(target);
             var newHolder = document.createElement("div");
             newHolder.setAttribute("class", klass);
             newHolder.appendChild(elem);
-            resultArea.replaceChild(newHolder, dummyHolder);
-            dummyHolder = newHolder;
+            target.appendChild(newHolder);
         }
 
-        function onUpdate(ev) {
-            var code = inputArea.value;
+        function onPatternUpdate(ev) {
+            var code = patternInputArea.value;
+
             try {
                 var parsed = EC.Parser.parse(code);
             } catch (x) {
-                echo(E.text(x), "error");
+                echoTo(x, patternStructureArea, "error");
                 return;
             }
 
-            echo(E.text(serialize(parsed)));
+            echoTo(serialize(parsed), patternStructureArea);
         }
 
-        inputArea.addEventListener("input", onUpdate, false);
+        function onValueUpdate(ev) {
+            try {
+                var value = eval("(" + valueInputArea.value + ")");
+            } catch (x) {
+                echoTo(x.message || "Failed to parse value", resultArea, "error");
+                return;
+            }
+            var pattern = patternInputArea.value;
+
+            var patterns = {};
+            patterns[pattern] = function (_) {
+                echoTo(serialize(_), resultArea);
+            };
+
+            patterns._ = function () {
+                echoTo("Not match", resultArea, "error");
+            };
+
+            EC.match(value, patterns);
+        }
+
+        patternInputArea.addEventListener("input", onPatternUpdate, false);
+        valueInputArea.addEventListener("input", onValueUpdate, false);
+
+        patternInputArea.addEventListener("input", onValueUpdate, false);
+        valueInputArea.addEventListener("input", onPatternUpdate, false);
     }, false);
 })();
